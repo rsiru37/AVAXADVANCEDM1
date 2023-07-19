a48b581ee1ae9268cde62b5c922d4e5fa8f509ab
rsiru37 <97042494+rsiru37@users.noreply.github.com>
2023-07-20 00:53:59 +0530

    Update subnet.sol
    Smart Contract that deploy a token and basic interaction

--- subnet.sol
+++ subnet.sol
@@ -1,5 +1,20 @@
-sfd
-//License
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract A is ERC20{
-  
+  constructor() ERC20("Ingameasset","IGA"){}
+
+function mont(address to,uint256 amt) public{
+        _mint(to,amt);
+    }
+
+function decimals() override public pure returns(uint8){
+        return 0;
+    }  
+
+function trans(address to,uint amt) public payable{
+        require(balanceOf(msg.sender)>=amt,"You dont have enough tokens to transfer");
+        approve(msg.sender,amt);
+        transferFrom(msg.sender,to,amt);
+    }
+}
